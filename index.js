80f8113fe49d55b3fd9a4984134ee0c75f279ca8
Sergey Panin <men865@gmail.com>
2021-10-01 15:15:18 +0300

    finish step2

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ export default () => {
   program
     .description('Compares two configuration files and shows a difference')
     .version('0.0.1')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1>')
+    .arguments('<filepath2>')
 
   program.parse(process.argv);
 };
