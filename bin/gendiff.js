74b868ded1ea858534bbe12a2f2bf0e6d26abb58
Sergey <men865@gmail.com>
2022-10-13 15:59:00 +0300

    fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import main from '../src/index.js'
+import main from '../src/index.js';
 
-main()
+main();
--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
-import _ from 'lodash';
-import fs from 'fs'
+import fs from 'fs';
 import { Command } from 'commander';
-const program = new Command();
 
-import path from 'path'
+import path from 'path';
+
+const program = new Command();
+const result = {};
+
+function readFile(file) {
+  return JSON.parse(fs.readFileSync(path.resolve(file), 'utf-8'));
+}
+
+function writeDifference(keys, json, json1, json2, operand) {
+  keys
+    .sort()
+    .forEach((key) => {
+      if (json1[key] === json2[key]) {
+        result[`  ${key}`] = json1[key];
+      } else {
+        result[`${operand} ${key}`] = json[key];
+      }
+    });
+}
+
+function checkFiles(firstFile, secondFile) {
+  const json1 = readFile(firstFile);
+  const json2 = readFile(secondFile);
+  const keys2 = Object.keys(json2);
+  const keys1 = Object.keys(json1);
+  writeDifference(keys1, json1, json1, json2, '-');
+  writeDifference(keys2, json2, json1, json2, '+');
+  console.log(result);
+}
 
 export default () => {
   program
@@ -12,34 +39,8 @@ export default () => {
     .version('0.0.1')
     .option('-f, --format <type>', 'output format')
     .arguments('<filepath1> <filepath2>')
-    .action(checkFiles)
+    .action(checkFiles);
   program.parse(process.argv);
 };
 
-const readFile = file => JSON.parse(fs.readFileSync(path.resolve(file), 'utf-8'))
-
-const writeDifference = (keys, json, json1, json2, operand, obj) => {
-  keys
-    .sort()
-    .forEach((key) => {
-      if (json1[key] == json2[key]) {
-        obj[`  ${key}`] = json1[key]
-      } else {
-        obj[`${operand} ${key}`] = json[key]
-      }
-    })
-}
-
-const checkFiles = (firstFile, secondFile) => {
-  const json1 = readFile(firstFile)
-  const json2 = readFile(secondFile)
-  const keys2 = Object.keys(json2)
-  const keys1 = Object.keys(json1)
-  const result = {}
-  writeDifference(keys1, json1, json1, json2, '-', result)
-  writeDifference(keys2, json2, json1, json2, '+', result)
-  console.log(result)
-}
-
-
-  // const options = program.opts();
+// const options = program.opts();
