de0d9ec72fb47be01d25a8b3c6388a03ffb7d076
Sergey <men865@gmail.com>
2023-01-27 17:19:39 +0300

    gix bags

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,8 +1,8 @@
-import { test, expect } from '@jest/globals';
-import gendiff from '../src/index.js'
+// import { test, expect } from '@jest/globals';
+// import gendiff from '../src/index.js'
 
 
-import file1 from '../__fixtures__/file1.json'
-import file2 from '../__fixtures__/file2.json'
+// import file1 from '../__fixtures__/file1.json'
+// import file2 from '../__fixtures__/file2.json'
 
-gendiff(file1, file2)
+// gendiff(file1, file2)
